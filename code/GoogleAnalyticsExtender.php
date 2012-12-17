1d3164630404923d3519a3b3af76c6da4b98937e
Lukas <lukas@lerni.ch>
2012-12-17 17:33:42 +0100

    Update code/GoogleAnalyticsExtender.php
    us optimized script from:
    http://mathiasbynens.be/notes/async-analytics-snippet

--- code/GoogleAnalyticsExtender.php
+++ code/GoogleAnalyticsExtender.php
@@ -1,20 +1,23 @@
 <?php
-class GoogleAnalyticsExtender extends DataObjectDecorator	{
+class GoogleAnalyticsExtender extends DataExtension {
 	function contentControllerInit($controller) {
 		$accountId = $this->owner->SiteConfig->GoogleAnalyticsAccountID;
+		$gVerification = $this->owner->SiteConfig->GoogleSiteVerification;
 		if(preg_match("/UA-[0-9]{7,}-[0-9]{1,}/", $accountId) &&  Director::isLive()) {
-			Requirements::customScript(<<<JS
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', '$accountId']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+Requirements::customScript(<<<JS
+var _gaq = [['_setAccount', '$accountId'], ['_trackPageview']];
+(function(d, t) {
+	var g = d.createElement(t),
+		s = d.getElementsByTagName(t)[0];
+	g.async = true;
+	g.src = '//www.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g, s);
+})(document, 'script');
 JS
-			);
+);
+		}
+		if($gVerification) {
+			Requirements::insertHeadTags('<meta name="google-site-verification" content="'. $gVerification .'" />');
 		}
 	}
 }
