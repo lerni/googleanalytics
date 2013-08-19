7dcbfaf7010e771304a273004d03b798bf17a2c8
Lukas Erni <lukaserni@Lukass-MacBook-Pro.local>
2013-08-19 14:03:35 +0200

    single quotes, readme
    no double quotes in tracking-code and updated readme about Universal
    Analytics

--- code/GoogleAnalyticsExtender.php
+++ code/GoogleAnalyticsExtender.php
@@ -9,13 +9,13 @@ class GoogleAnalyticsExtender extends DataExtension {
 		if(preg_match("/UA-[0-9]{7,}-[0-9]{1,}/", $accountId) &&  Director::isLive()) {
 		Requirements::insertHeadTags(sprintf(
 			'<script>
-  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+  (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+  })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');
 
-  ga("create", "%s", "%s");
-  ga("send", "pageview");
+  ga(\'create\', \'%s\', \'%s\');
+  ga(\'send\', \'pageview\');
 
 </script>', $accountId, $gDomain
 		));
