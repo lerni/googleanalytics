35b0a81c73c7f93b0e3e7da8d1f0b98dd66f40c7
lerni <lukas@lerni.ch>
2018-05-02 09:22:10 +0200

    per composer, per template & GDPR/DSGVO

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Google Analytics (Universal Analytics)
+# Google Analytics
 
 ## Requirements
 
@@ -11,12 +11,8 @@ Download, place the folder "googleanalytics" in your project root and run a dev/
 
 ## Usage Overview
 
-A module to add Google Trackingcode (Universal Analytics) & Siteverification Metatag per SiteConfig in CMS
+A module to add Google Trackingcode per SiteConfig in CMS
 
 
-## Known Issues
-
+## Known Issues / Hints
 * Trackingcode is only shown in live-mode.
-* The name of the Web-Property (see: https://support.google.com/analytics/answer/2790010?hl=en&ref_topic=2790009 )is assumed to be the domain-name without www-Prefix.
-* Probable the Tracking Code should be in a Template-File and not in php.
-* ATM there is no way to "upgrade" a profile to "Universal Analytics": http://stackoverflow.com/questions/16528899/upgrading-to-google-universal-analytics
--- code/GoogleAnalyticsExtender.php
+++ code/GoogleAnalyticsExtender.php
@@ -2,22 +2,13 @@
 class GoogleAnalyticsExtender extends DataExtension {
 	function contentControllerInit($controller) {
 		$accountId = $this->owner->SiteConfig->GoogleAnalyticsAccountID;
-		$gDomain = 'auto';
+		$arrayData = new ArrayData(array(
+			'accountId' => $accountId
+		));
 		if(preg_match("/UA-[0-9]{7,}-[0-9]{1,}/", $accountId) &&  Director::isLive()) {
 			if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false) {
-				Requirements::insertHeadTags(sprintf(
-					'<script>
-  (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');
-
-  ga(\'create\', \'%s\', \'%s\');
-  ga(\'require\', \'displayfeatures\');
-  ga(\'send\', \'pageview\');
-
-</script>', $accountId, $gDomain
-				));
+				$analyticsString = $arrayData->renderWith('Ananlytics');
+				Requirements::insertHeadTags($analyticsString);
 			}
 		}
 	}
