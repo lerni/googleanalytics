d6930bdb50ea67d8451c670896d2df4212214ef6
lerni <lukas@lerni.ch>
2019-05-28 15:55:52 +0200

    not detect Speed Insights anymore, use Extension not DataExtension, SiteConfig::current_site_config instead  ->owner->SiteConfig

--- code/GoogleAnalyticsExtender.php
+++ code/GoogleAnalyticsExtender.php
@@ -1,17 +1,16 @@
 <?php
-class GoogleAnalyticsExtender extends DataExtension {
+class GoogleAnalyticsExtender extends Extension {
 	function contentControllerInit($controller) {
-		$accountId = $this->owner->SiteConfig->GoogleAnalyticsAccountID;
-		$GTMAccountId = $this->owner->SiteConfig->GTMAccountID;
+		$config = SiteConfig::current_site_config();
+		$accountId = $config->GoogleAnalyticsAccountID;
+		$GTMAccountId = $config->GTMAccountID;
 		$arrayData = new ArrayData(array(
 			'accountId' => $accountId,
 			'GTMAccountId' => $GTMAccountId
 		));
 		if(preg_match("/UA-[0-9]{7,}-[0-9]{1,}/", $accountId) &&  Director::isLive()) {
-			if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false) {
-				$analyticsString = $arrayData->renderWith('Ananlytics');
-				Requirements::insertHeadTags($analyticsString);
-			}
+			$analyticsString = $arrayData->renderWith('Ananlytics');
+			Requirements::insertHeadTags($analyticsString);
 		}
 	}
 }
